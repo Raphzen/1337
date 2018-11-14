50a2162348c9ded0171c5a870c53e7891cff8fdd
Raphzen <raphael.weber@fiveweber.de>
2018-11-14 19:01:35 +0100

    l

--- Lichtwecker.py
+++ Lichtwecker.py
@@ -25,15 +25,15 @@ LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-Wake_Up   = "19:00:00"
+Wake_Up   = "04:50:00"
 
 
            
 def Lichtwecker(strip):
     x=0
     for start_time in range(0, 255):
-        print(start_time)
-        print(time.strftime("%X"))
+        #print(start_time)
+        #print(time.strftime("%X"))
         for i in range(0, strip.numPixels()):
             strip.setPixelColor(i, Color(255,209,163))
             strip.setBrightness(x)
@@ -67,10 +67,10 @@ if __name__ == '__main__':
         while True:
             current_day=datetime.datetime.today().weekday()
             current_time=str(time.strftime("%X"))
-            print(current_time)
-            print(current_day)
+            #print(current_time)
+            #print(current_day)
             if current_time == Wake_Up and current_day in (0,1,2,3,4):
-                print("HI")
+                #print("HI")
                 Lichtwecker(strip)
             if current_time == "05:30:00":
                 for i in range(0, strip.numPixels()):
