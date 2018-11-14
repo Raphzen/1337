7b945fa9b55d7c05e52d0a84c754327d3c5c6260
Raphzen <raphael.weber@fiveweber.de>
2018-11-14 18:12:48 +0100

    l

--- Lichtwecker.py
+++ Lichtwecker.py
@@ -25,7 +25,7 @@ LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-Wake_Up   = "18:08:00"
+Wake_Up   = "18:14:00"
 
 
            
