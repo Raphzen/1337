b87c479817056b6275de13e4005fd4733dc4d953
Raphzen <raphael.weber@fiveweber.de>
2018-11-17 00:42:23 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -74,7 +74,7 @@ def thunder(strip, wait_ms=numpy.random.uniform(0.01,0.1)):
 def Mostly_Cloudy(strip):
     Color_Array = []
     run_once=0
-    strip.setPixelColor(1, Color(1,50,250))
+    strip.setPixelColor(1, Color(0,0,250))
     if run_once==0:
         for i in range(0, strip.numPixels()):
             Color_Array.append(i)
