6850adb5a5c3ec913f6b80609093b2cea4a87346
Raphzen <raphael.weber@fiveweber.de>
2018-11-21 20:26:35 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -43,18 +43,18 @@ def weather():
     Current_Conditions=weather_com_result['current_conditions']['text']
 
 
-def Static():
-    V0=Blynk(auth_token, pin="V0")
-    V1=Blynk(auth_token, pin="V1")
-    V2=Blynk(auth_token, pin="V2")
-    V0_val=V0.get_val()
-    V1_val=int(V1.get_val())
-    V2_val=int(V2.get_val())
+#def Static():
+#    V0=Blynk(auth_token, pin="V0")
+#    V1=Blynk(auth_token, pin="V1")
+#    V2=Blynk(auth_token, pin="V2")
+#    V0_val=V0.get_val()
+#    V1_val=int(V1.get_val())
+#    V2_val=int(V2.get_val())
+#
 
-
-    for i in range(0, strip.numPixels()):
-        strip.setPixelColor(i,Color(V0_val,V1_val,V2_val))
-    strip.show()
+#    for i in range(0, strip.numPixels()):
+#        strip.setPixelColor(i,Color(V0_val,V1_val,V2_val))
+#    strip.show()
 
 
 def flash(strip, wait_ms=50):
