cbb7afe3256b66a111c063b27f9d840bbc9fadd5
Raphzen <raphael.weber@fiveweber.de>
2018-12-01 18:36:00 +0100

    L

--- Thunder.py
+++ Thunder.py
@@ -66,16 +66,15 @@ def Rain(strip):
             Color_Array[i]=Color(0,0,0)
             strip.setPixelColor(i, Color_Array[i])
         run_once=1
-    while 1:
-        Rain_x=randint(40,50)
-        for l in range(0, Rain_x):
-            for m in range(0, strip.numPixels()):
-                strip.setPixelColor(m, Color_Array[m])
-            strip.show()
-            Color_Array = (Color_Array[len(Color_Array) - 1:len(Color_Array)]  #weiterschieben
-                    + Color_Array[0:len(Color_Array) - 1]) 
-            time.sleep(25/1000.0)
-        Color_Array[1]=Color(13,80,250)
+    Rain_x=randint(40,50)
+    for l in range(0, Rain_x):
+        for m in range(0, strip.numPixels()):
+            strip.setPixelColor(m, Color_Array[m])
+        strip.show()
+        Color_Array = (Color_Array[len(Color_Array) - 1:len(Color_Array)]  #weiterschieben
+                + Color_Array[0:len(Color_Array) - 1]) 
+        time.sleep(25/1000.0)
+    Color_Array[1]=Color(13,80,250)
 
 def flash(strip, wait_ms=50):
     for i in range(0, strip.numPixels()):
