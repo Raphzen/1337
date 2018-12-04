a0940fcd2fca13ab267e82383f60e3cdd419be03
Raphzen <raphael.weber@fiveweber.de>
2018-12-04 19:25:27 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -166,16 +166,16 @@ if __name__ == '__main__':
         start_time=0
         while True:
             current_time=time.time()
-            #if (current_time-start_time)>3:
-                # # # start_time=time.time()
-                # # # weather_com_result=pywapi.get_weather_from_weather_com('SNXX0006')
-                # # # temperature=int(weather_com_result['current_conditions']['temperature'])
-                # # # temp_f=temperature * 9 / 5 + 32
-                # # # humidity=int(weather_com_result['current_conditions']['humidity'])
-                # # # Current_Conditions=weather_com_result['current_conditions']['text']
-                # # # #beaufort = int(weather_com_result['wind']['speed'])
-                # # # print("Aktuelles Wetter: " + Current_Conditions)
-                # # # #print("Windgeschwindigkeit: " + beaufort)
+            if (current_time-start_time)>3:
+                start_time=time.time()
+                weather_com_result=pywapi.get_weather_from_weather_com('SNXX0006')
+                temperature=int(weather_com_result['current_conditions']['temperature'])
+                temp_f=temperature * 9 / 5 + 32
+                humidity=int(weather_com_result['current_conditions']['humidity'])
+                Current_Conditions=weather_com_result['current_conditions']['text']
+                #beaufort = int(weather_com_result['wind']['speed'])
+                print("Aktuelles Wetter: " + Current_Conditions)
+                #print("Windgeschwindigkeit: " + beaufort)
 
             #Current_Conditions="Rain"
 
