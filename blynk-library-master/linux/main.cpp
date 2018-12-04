94f112b441dec326225e4185de709c4df46bcdbb
Raphzen <raphael.weber@fiveweber.de>
2018-12-04 21:52:57 +0100

    l

--- blynk-library-master/linux/main.cpp
+++ blynk-library-master/linux/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
         loop();
     }
 
-
+//Numpy Array Übergabe
 PyModINIT_FUNC
 inittestFunction(void){
    (void) Py_InitModule("testFunction". testFunctionMethods);
