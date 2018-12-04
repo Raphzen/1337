75427b35bfb35d39289527726dd0f6e3d62cf2a0
Raphzen <raphael.weber@fiveweber.de>
2018-12-04 18:08:47 +0100

    Blynk

--- Thunder.c
+++ Thunder.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
 
 int main() {
+    Blynk();
 
-printf("Hello World\n");
-//Run getweather
+    GetWeather();
 
+    RunLED();
 //Run LED
 
-//BLYNK
+int Blynk(){
+    Blynk.begin();
+
+    BLYNK_AUTH="e9923f06747e4e44a698f04324c909ec"
+
+    BLYNK_WRITE(V0){
+        int r=param.asInt();
+        printf(r"\n");
+    }
+}
 
 }
 
