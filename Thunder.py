d0e6eff97920fa17089a5f5e305ea4a9ff5dc909
Raphzen <raphael.weber@fiveweber.de>
2018-12-05 14:08:25 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
             #Current_Conditions="Rain"
             ### Aktuellen Wetter Modus speichern: wenn der Modus==den Current Conditions:
                 ### kein erneuter Funktionsaufruf wegen Zuruecksetzen der LEDs
-            if Actual_Mode<>"T-Storm":    
+            if Actual_Mode!="T-Storm":    
                 if Current_Conditions=="T-Storm":
                     Actual_Mode="T-Storm"
                     wait_ms=randint(2, 10)
@@ -259,7 +259,7 @@ if __name__ == '__main__':
                     thunder(strip)
                 if randint(0,10) >8:
                     thunder(strip)
-            if Actual_Mode<>"T-Storm":         
+            if Actual_Mode!="T-Storm":         
                 if Current_Conditions=="Mostly Cloudy":
                     Actual_Mode="Mostly Cloudy"
                     Mostly_Cloudy(strip)
