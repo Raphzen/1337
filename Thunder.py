bd3a67d3e1fc59037f174065fdf83695ba3dd515
Raphzen <raphael.weber@fiveweber.de>
2018-12-14 23:52:00 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -30,6 +30,7 @@ LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+global strip
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, strip_type=ws.WS2811_STRIP_GRB)
 
 
