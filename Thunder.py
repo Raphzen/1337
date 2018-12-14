5a02ba28f8c507be6383ee920860fbb7f0ebdf6e
Raphzen <raphael.weber@fiveweber.de>
2018-12-14 23:43:41 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -27,44 +27,23 @@ from flask_restful import Api, Resource , reqparse
 app = Flask(__name__)
 api = Api(app)
 
-class controller(Resource):
-    # LED strip configuration:
-    LED_COUNT      = 144      # Number of LED pixels.
-    LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-    #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-    LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-    LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
-    LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
-    LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-    global strip
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, strip_type=ws.WS2811_STRIP_GRB)
+@app.route('/State/<string:Value>')
+def State(Value):
+    print("Fick Dich!!!!!")
 
-    global Off
-    def Off(strip):
-        for i in range(0, strip.numPixels()):
-            strip.setPixelColor(i,Color(0,0,0))
-        strip.show()
-
-    global On
-    def On(strip):
-        for i in range(0, strip.numPixels()):
-            strip.setPixelColor(i,Color(255,255,255))
-        strip.show()
-
-    Off(strip)
-    def get(self,status):
-        if (status == "On" ):
-            On(strip)
-            return "superOn" ,200
-        if (status == "Off" ):
-            Off(strip)
-            return "superOff" ,200
-        return "scheisseOff" , 404
+# class controller(Resource):
+#     def get(self,status):
+#         if (status == "On" ):
+#             On(strip)
+#             return "superOn" ,200
+#         if (status == "Off" ):
+#             Off(strip)
+#             return "superOff" ,200
+#         return "scheisseOff" , 404
 
 
-api.add_resource(controller, "/TurnOn/<string:status>")
+# # api.add_resource(controller, "/TurnOn/<string:status>")
 app.run(debug=True)
 
 
@@ -108,6 +87,16 @@ def Static(strip):
 #for i in range(0, LED_COUNT-1):
 #    Color_Array.append(i)
 
+
+def Off(strip):
+    for i in range(0, strip.numPixels()):
+        strip.setPixelColor(i,Color(0,0,0))
+    strip.show()
+
+def On(strip):
+    for i in range(0, strip.numPixels()):
+        strip.setPixelColor(i,Color(255,255,255))
+    strip.show()
 
 
 def Reset(strip):
