3dd3cd2922932e0d92cd7ac66b9e76b73c470e56
Raphzen <raphael.weber@fiveweber.de>
2018-12-14 21:11:27 +0100

    l

--- Thunder.py
+++ Thunder.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
                 if randint(0,10) >8:
                     thunder(strip)
             if Actual_Mode!="Mostly Cloudy":  ### schriebt einmal den Funktionsaufruf. Im aufruf keine schleife.
-                                                ### schleife wieder einführen und bei neuem Actual Mode einen Break des Loops       
+                                                ### schleife wieder einfuehren und bei neuem Actual Mode einen Break des Loops       
                 if Current_Conditions=="Mostly Cloudy":
                     Actual_Mode="Mostly Cloudy"
                     Mostly_Cloudy(strip)
