4e498d135e324d85a1787efe3b3ab09b078b49cf
Raphzen <raphael.weber@fiveweber.de>
2018-12-23 15:29:51 +0100

    l

--- Thunder5.py
+++ Thunder5.py
@@ -185,7 +185,7 @@ def State_Do(value, Mode):
     if (Mode!=Actual_Mode):
         if (value!="Mostly_Cloudy"):
             MC=Process(target=Mostly_Cloudy, args=(strip, value,))
-            MC.exit()
+            MC.Event()
             MC.join
         if (Mode=="OFF"):
             Off(strip, value)
